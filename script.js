1e1b601eb3e23c9a4bd7afa03c1fd77158b5bdb5
Petr Nikitin <nikitablvck@gmai.com>
2020-05-10 16:59:53 +0300

    filter millionaires with array.filter

--- script.js
+++ script.js
@@ -50,14 +50,30 @@ function updateDOM(providedData = data) {
 function formatMoney(number) {
   return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
-
+// Double money Users
 function doubleMoney() {
   data = data.map((user) => {
     return { ...user, money: user.money * 2 };
   });
   updateDOM();
 }
+
+// Sort Users by Richest
+
+function sortByRichest() {
+  data = data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
+// filter only millionaires
+
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+}
 //Event Listeners
 
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
