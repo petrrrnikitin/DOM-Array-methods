5f643639b193c221c32472c9a53de5a249640c88
Petr Nikitin <nikitablvck@gmai.com>
2020-05-10 15:22:03 +0300

    output users - forEach() & DOM Methods

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
-const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
@@ -14,21 +14,41 @@ getRandomUser();
 // fetch random user and add money
 
 async function getRandomUser() {
-  const res = await fetch('https://randomuser.me/api');
+  const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
   const user = data.results[0];
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
-  }
+  };
 
   addData(newUser);
 }
 
-
 //Add new obj to data arr
 
-addData(user) {
+function addData(user) {
   data.push(user);
+  updateDOM();
 }
+
+function updateDOM(providedData = data) {
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+}
+
+// Format wealth number to money
+
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners
+
+addUserBtn.addEventListener('click', getRandomUser)
